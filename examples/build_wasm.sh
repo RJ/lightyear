2b610910b541f9eb07a1c081a472f98be464695c
RJ <rj@metabrew.com>
2024-12-14 16:26:05 +0000

    faster to build multiple with one cargo invocation, only builds deps once

--- examples/build_wasm.sh
+++ examples/build_wasm.sh
@@ -6,7 +6,7 @@ BASE_DIR=$(builtin cd .. ; pwd)
 EXAMPLES_DIR="$BASE_DIR/examples"
 HTDOCS_DIR="$BASE_DIR/htdocs"
 CARGO_RELEASE_DIR="$BASE_DIR/target/wasm32-unknown-unknown/release"
-
+export RUSTFLAGS=--cfg=web_sys_unstable_apis
 export CARGO_BUILD_TARGET=wasm32-unknown-unknown
 
 # load "example_list" variable from .env file, shared with Dockerfile.server
@@ -38,16 +38,25 @@ fi
 export TARGET_CC
 echo "Using clang at $TARGET_CC"
 
+# we want to compile all examples in the list with one cargo command, which is much faster,
+# becayse it only builds the shared deps once.
+# build up a string of "-p example1 -p example2" etc to pass to cargo.
+p_args=""
+for example in $(echo $example_list | tr ',' ' ') ;
+do
+	p_args="$p_args -p $example"
+done
 
+echo "Building examples: $p_args"
+cargo build --release --no-default-features -F bevygap_client $p_args
 
+echo "Running wasm-bindgen for each example"
 for example in $(echo $example_list | tr ',' ' ') ;
 do
 	(
 	outdir="$HTDOCS_DIR/$example"
 	mkdir -p "$outdir"
-	echo "Building $example into '$outdir'"
 	cd "$EXAMPLES_DIR/$example"
-	cargo build --release --no-default-features -F bevygap_client
 	wasm-bindgen --no-typescript --target web --out-dir "$outdir" --out-name "$example" "$CARGO_RELEASE_DIR/$example.wasm"
 	sed -e "s/{{name}}/$example/g" "$EXAMPLES_DIR/common/www/index.html" > "$outdir/index.html"
 	)
