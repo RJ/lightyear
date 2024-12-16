d37e0ccecb5ed8379a340ac037f9a88c5655276d
RJ <rj@metabrew.com>
2024-12-16 19:43:41 +0000

    try -j 2

--- examples/build_wasm.sh
+++ examples/build_wasm.sh
@@ -11,9 +11,8 @@ export CARGO_BUILD_TARGET=wasm32-unknown-unknown
 
 if [[ ! -z "$RUNNER_OS" ]]; then
     # an attempt to get the github runner to not kill us due to resource usage
-    echo "Running on github runner, setting CARGO_BUILD_JOBS=1"
-    # runner has 4 cores, so let's try 3 concurrent jobs?
-    export CARGO_BUILD_JOBS=3
+    echo "Running on github runner, setting CARGO_BUILD_JOBS lower"
+    export CARGO_BUILD_JOBS=2
 fi
 
 # load "example_list" variable from .env file, shared with Dockerfile.server
